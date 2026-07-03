6be6721582bd32f9c45fb3b95a37ff8c7cf23d39
federicamessi2000 <federicamessi2000@hotmail.com>
2026-07-03 15:18:27 +0200

    sw: bump cache to v7 per forzare aggiornamento PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'cucina-v6';
+var CACHE = 'cucina-v7';
 var PRECACHE = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e) {
